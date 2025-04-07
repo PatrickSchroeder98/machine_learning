214fae4cf82e2aeacab56838fbe6c3844af9ff2d
Patrick Schroder <patrickschroder479@gmail.com>
2025-04-07 19:11:19 +0200

    Fixed missing data in the exercise
    Added function and modified data frame to be valid for logistic regression.

--- course_exercise_logistic_regression/main.py
+++ course_exercise_logistic_regression/main.py
@@ -38,3 +38,45 @@ plt.show()
 plt.figure(figsize=(10, 7))
 sns.boxplot(x='Pclass', y='Age', data=df)
 plt.show()
+
+
+# Fixing missing data
+def impute_age(cols):
+    """Function that fills the missing age numbers based on average values from one of 3 classes. """
+    age = cols[0]
+    pclass = cols[1]
+
+    if pd.isnull(age):
+        if pclass == 1:
+            return 37
+        elif pclass == 2:
+            return 29
+        else:
+            return 24
+    else:
+        return age
+
+
+df['Age'] = df[['Age', 'Pclass']].apply(impute_age, axis=1)
+
+sns.heatmap(df.isnull(), yticklabels=False, cbar=False, cmap='viridis')
+plt.show()
+
+# The column 'Cabin' and then rows has been dropped
+df.drop('Cabin',axis=1,inplace=True)
+df.dropna(inplace=True)
+
+sns.heatmap(df.isnull(), yticklabels=False, cbar=False, cmap='viridis')
+plt.show()
+
+gender = pd.get_dummies(df['Sex'], drop_first=True)
+embark = pd.get_dummies(df['Embarked'], drop_first=True)
+
+train = pd.concat([df, gender, embark], axis=1)
+train.drop(['Sex', 'Embarked', 'Name', 'Ticket'], axis=1, inplace=True)
+train.drop('PassengerId', axis=1, inplace=True)
+
+print(train.head())  # Data has been prepared for ML operations
+
+
+# TODO update README
