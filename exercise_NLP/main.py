9ff9e237410a2e3a5e6fd1063d9fab5c3ba0a531
Patrick Schröder <patrickschroder479@gmail.com>
2025-09-28 13:08:00 +0200

    Updated NLP exercise
    Added code for text pre-processing.

--- exercise_NLP/main.py
+++ exercise_NLP/main.py
@@ -2,6 +2,9 @@ import nltk
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import string
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer
 
 # nltk.download()
 
@@ -30,3 +33,38 @@ print(messages[messages['length'] == 910]['message'].iloc[0])
 
 messages.hist(column='length', by='label', bins=50,figsize=(12,4))
 plt.show()
+
+# Text pre-processing
+
+def text_process(mess):
+    """
+    Takes in a string of text, then performs the following:
+    1. Remove all punctuation
+    2. Remove all stopwords
+    3. Returns a list of the cleaned text
+    """
+    # Check characters to see if they are in punctuation
+    nopunc = [char for char in mess if char not in string.punctuation]
+
+    # Join the characters again to form the string.
+    nopunc = ''.join(nopunc)
+
+    # Now just remove any stopwords
+    return [word for word in nopunc.split() if word.lower() not in stopwords.words('english')]
+
+print(messages['message'].head(5).apply(text_process))
+
+bow_transformer = CountVectorizer(analyzer=text_process).fit(messages['message'])
+
+# Print total number of vocab words
+print(len(bow_transformer.vocabulary_))
+
+message4 = messages['message'][3]
+print(message4)
+
+bow4 = bow_transformer.transform([message4])
+print(bow4)
+print(bow4.shape)
+
+print(bow_transformer.get_feature_names_out()[4068])
+print(bow_transformer.get_feature_names_out()[9554])
